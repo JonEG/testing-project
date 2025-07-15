97081c445e9fbed5bffa735927bc862481696357
Jon Echeveste González <j.echeveste-gonzales@ovan.de>
2025-07-15 17:40:50 +0100

    Banner, implemented close button

--- packages/sitepackage/ContentBlocks/ContentElements/banner/main.ts
+++ packages/sitepackage/ContentBlocks/ContentElements/banner/main.ts
@@ -1,1 +1,10 @@
-console.log("This is the banner Typescript");
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll<HTMLElement>('[data-banner]').forEach(banner => {
+    const closeButton = banner.querySelector<HTMLButtonElement>('[data-banner-close-button]');
+    if (!closeButton) return;
+
+    closeButton.addEventListener('click', () => {
+      banner.style.display = 'none';
+    });
+  });
+});
